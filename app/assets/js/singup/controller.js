03649dc36d5012995ceb05ee83750ea67bf4a89a
Miguel Costero <miguelcostero@outlook.com>
2016-07-14 16:47:12 -0400

    introducing to personal tastes

--- app/assets/js/singup/controller.js
+++ app/assets/js/singup/controller.js
@@ -1,24 +1,21 @@
 var app = angular.module("pritvApp");
 
-app.controller("SingUpController", function ($scope, $rootScope, planes) {
-  $scope.formData = {};
-  $scope.years = [];
+app.controller("SingUpController", function($scope, $rootScope, planes, generos) {
+    $scope.formData = {};
+    $scope.years = [];
 
-  planes.get({}, {}, function (data) {
-    $scope.planes = data;
-  });
+    planes.get({}, {}, function(data) {
+        $scope.planes = data;
+    });
 
-  var myDate = new Date();
-  var year = myDate.getFullYear();
-  for(var i = year; i < 2046; i++) {
-    $scope.years.push({"year": i});
-  }
+    generos.get({}, {}, function (data) {
+        $scope.generos = data;
+    })
 
-/*
-  if ($scope.formData.profile.nombre && $scope.formData.profile.apellido && $scope.formData.profile.email && $scope.formData.profile.fecha_nacimiento && $scope.formData.profile.direccion && $scope.formData.profile.telefono && $scope.formData.subscription.id_tipo_suscripcion  && $scope.formData.subscription.nombre && $scope.formData.subscription.descripcion && $scope.formData.subscription.monto_mensual && $scope.formData.subscription.num_usuarios && $scope.formData.payment.tipo && $scope.formData.payment.card && $scope.formData.payment.cvv && $scope.formData.payment.vencimiento && $scope.formData.user.nickname && $scope.formData.user.bio && $scope.formData.user.imagen) {
+    var myDate = new Date();
+    var year = myDate.getFullYear();
+    for (var i = year; i < 2046; i++) {
+        $scope.years.push({ "year": i });
+    }
 
-    $scope.formValidate = true;
-  } else {
-    $scope.formValidate = false;
-  } */
 });
